4fffee01df24b050685a0b8f99f2e5cf5d23c86e
juaca2oo4 <juacaoo39@gmail.com>
2023-04-24 21:03:53 -0500

    upload loadPassenger

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -2,6 +2,32 @@ package model;
 
 public class Controller {
 
-    
-    
+    private HashTable<String, Passenger> hashTable;
+
+    public Controller() {
+
+    }
+
+    public void LoadPassenger(String txt, int count) {
+        File file = new File(txt);
+        hashTable = new HashTable<>(count);
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                Passenger p = new Passenger(parts[0], Boolean.parseBoolean(parts[1]), Integer.parseInt(parts[2]),
+                        Integer.parseInt(parts[3]));
+
+                hashTable.insert(parts[0], p);
+            }
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
